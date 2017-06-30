5f480a08e8bd3fc54597c2cec67dae04b987b007
miquelalcon <miquel.1996@gmail.com>
2017-06-30 09:06:22 +0200

    after 29-7-17 class

--- database/seeds/user.seed.js
+++ database/seeds/user.seed.js
@@ -3,11 +3,6 @@ const users = [
         email: "miguel.alcon@est.fib.upc.edu",
         password: process.env.PWD_SUPERUSER,
         username: "miquelalcon"
-    },
-    {
-        email: "guest@quotes.guest",
-        password: "guest",
-        username: "guest"
     }
 ]
 
--- utils/http-response.js
+++ utils/http-response.js
@@ -7,27 +7,34 @@ class HttpResponse {
 
     badRequest (res, msg) {
         if (!msg || process.env.ENV == "production")
-            msg = {message: "The request was malformed or invalid"}
+            msg = {message: "The request was malformed or invalid."}
         res.status(400).json(msg)
     }
 
     notFound (res, msg) {
         if (!msg || process.env.ENV == "production")
-            msg = {message: "Resource not found"}
+            msg = {message: "Resource not found."}
         res.status(404).json(msg)
     }
 
     error (res, msg) {
         if (!msg || process.env.ENV == "production")
-            msg = {message: "Internal server error"}
+            msg = {message: "Internal server error."}
         res.status(500).json(msg)
     }
 
     unauthorized (res, msg) {
         if (!msg || process.env.ENV == "production")
-            msg = {message: "The authentification for this resource was not successful"}
+            msg = {message: "The authentification for this resource was not successful."}
         res.status(401).json(msg)
     }
+
+    // conflict (res, msg) {
+    //     if (!msg || process.env.ENV == "production")
+    //         msg = {message: "The request could not be completed due to a " +
+    //                         "conflict with the current state of the resource."}
+    //     res.status(409).json(msg)
+    // }
 }
 
 module.exports = new HttpResponse()
